7426ee40a7eeb7bc2c851b04057f9bdd0280c15b
krigsmahtana <krigsmahtana@gmail.com>
2021-04-16 21:59:00 +0300

    исправления

--- oop/Figure.py
+++ oop/Figure.py
@@ -1,56 +1,109 @@
-#тест
-# General class
-from oop.Rectangle import Rectangle
-from oop.Circle import Circle
-from oop.Square import Square
-from oop.Triangle import Triangle
+# тест
+import math
 
 
 class Figure:
-    _instance = None
-    # просто тестовые данные
-    a = 3
-    b = 8
-    c = 7
+    pass
 
-    # Работа с треугольником
-    triangle = Triangle(a, b, c)
-    triangle.get_name()
-    triangle.area()
-    triangle.perimeter()
-    triangle.angles()
 
-    # работа с окружностью
-    circle = Circle(r=a)
-    circle.get_name()
-    circle.area()
-    circle.perimeter()
-    circle.angles()
+class Square(Figure):
+    name = 'Square'
 
-    # работа с квадратом
-    square = Square(a)
-    square.get_name()
-    square.area()
-    square.perimeter()
-    square.angles()
+    def __init__(self, a):
+        self.a = a
 
-    # работа с прямоугольником
-    rectangle = Rectangle(a, b)
-    rectangle.get_name()
-    rectangle.area()
-    rectangle.perimeter()
-    rectangle.angles()
+    def get_name(self):
+        return self.name
 
-    # складывание площадей
+    def area(self):
+        s = self.a ** 2
+        return s
+
+    def angles(self):
+        ang = 4
+        return ang
+
+    def perimeter(self):
+        d = 4 * self.a
+        return d
+
+
+class Rectangle(Figure):
+    name = 'Rectangle'
+
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+    def get_name(self):
+        return self.name
+
+    def area(self):
+        s = self.a * self.b
+        return s
+
+    def angles(self):
+        ang = 4
+        return ang
+
+    def perimeter(self):
+        d = (2 * self.a) + (2 * self.b)
+        return d
+
+
+class Triangle(Figure):
+    name = 'Triangle'
+
+    def __init__(self, a, b, c):
+        self.a = a
+        self.b = b
+        self.c = c
+
+    def get_name(self):
+        return self.name
+
+    def area(self):
+        p = (self.c + self.b + self.a) / 2
+        s = math.sqrt((p * (p - self.c) * (p - self.b) * (p - self.a)))
+        return s
+
+    def angles(self):
+        ang = 3
+        return ang
+
+    def perimeter(self):
+        d = self.c + self.b + self.a
+        return d
+
+
+class Circle(Figure):
+    name = 'Circle'
+
+    def __init__(self, r):
+        self.r = r
+
+    def get_name(self):
+        return self.name
+
+    def area(self):
+        s = math.pi * (self.r ** 2)
+        return s
+
+    def angles(self):
+        ang = 0
+        return ang
+
+    def perimeter(self):
+        d = 2 * self.r * math.pi
+        return d
+
+
 def add_area(area_one, area_two):
     area_one = area_one
     area_two = area_two
 
     try:
         sum_area = area_one + area_two
-        print(sum_area)
-        return  sum_area
-    except ValueError:
-        print("передан неправильный класс")
-
-
+        return sum_area
+    except Exception:
+        return "передан неправильный класс"
